eaff2aa9858fd2420ddf189ca05d02d84e09ae26
Virat Kumar <viratkumar@Virats-MacBook-Air.local>
2024-10-07 11:51:46 +0530

    2nd commit

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var elemC=document.querySelector("#elem-cont")
 var fixed=document.querySelector("#fixed-img")
 elemC.addEventListener("mouseenter",function(){
     fixed.style.display="block"
-})
+}) 
 elemC.addEventListener("mouseleave",function(){
     fixed.style.display="none"
 })
